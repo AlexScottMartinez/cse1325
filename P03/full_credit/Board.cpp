1aadd2dfe814b8ba8403459535294c9fc88a6fc1
Alexander Martinez <alexander.martinez2@mavs.uta.edu>
2020-09-14 15:27:50 -0500

    first commit but not finished

---
+++ P03/full_credit/Board.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+class Board {
+	vector<Tile> _tiles  {
+	"bang", "beam", "bear", "cafe", "came",
+	"cold", "come", "cool", "date", "diet",
+	"door", "doth", "echo", "exes", "exit",
+	"face", "fame", "feed", "fire", "five",
+	"four", "free", "frog", "from", "game",
+	"goes", "good", "grit", "have", "heal",
+	"hear", "here", "hide", "hole", "home",
+	"hope", "huge", "keep", "kill", "lace",
+	"lame", "leek", "life", "live", "maps",
+	"mass", "mate", "maze", "move", "near",
+	"need", "nine", "once", "quit", "ques",
+	"race", "read", "reed", "reef", "rice",
+	"roof", "sins", "stay", "tame", "teen",
+	"tens", "tide", "tied", "tree", "twin",
+	"zero", "zips",
+	};
+
+Public:
+	Board(tiles: int)
+	string attempt(tile1: int,tile2: int) {
+        if
+    }
+	bool solved() {
+		if (){
+            return (true);
+        }
+        else {
+            return (false);
+        }
+	}
+	string to_string() {
+		return (Board); 
+	}
+
+};
