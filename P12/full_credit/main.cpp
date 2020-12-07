dd181776a030d0ac84bb2a2795c2df00dc490a50
Alexander Martinez <alexander.martinez2@mavs.uta.edu>
2020-12-07 11:37:52 -0600

    Second Commit

---
+++ P12/full_credit/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	std::cout << "   width: " << argv[1] << "   height: " << argv[2] << "   icount: " << argv[3] << "   threads: " << argv[4] << "\n   wrote: " << argv[5] << std::endl;
+
+}
